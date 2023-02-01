e8d7dc22449bdff67b998886a960ce3d8ed30b4c
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2023-02-01 20:55:26 +0300

    MediatR-Read AppDbContext & Migration

--- EventSourcing.API/Program.cs
+++ EventSourcing.API/Program.cs
@@ -1,11 +1,16 @@
 using EventSourcing.API.EventStores;
+using EventSourcing.API.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
