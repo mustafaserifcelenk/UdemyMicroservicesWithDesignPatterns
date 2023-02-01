1de18a3766baa9b080ea97bab51c0f8e2f2f2303
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2023-02-01 20:07:09 +0300

    Product Stream

--- EventSourcing.API/Program.cs
+++ EventSourcing.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEventStore(builder.Configuration);
+builder.Services.AddSingleton<ProductStream>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
